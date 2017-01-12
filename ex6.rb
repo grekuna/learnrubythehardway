644251e6dc17e5bbd17440991e77b3f4cc55c406
grekuna <gregor.martius@gmail.com>
2017-01-12 11:23:26 +0100

    Finish exercise 6

---
+++ ex6.rb
@@ -0,0 +1,30 @@
+#assings the value 10 to the variable
+types_of_people = 10
+#assings the value to the variable
+x = "There are #{types_of_people} types of people"
+#assings the value to the variable
+binary = "binary"
+#assings the value to the variable
+do_not = "don't"
+#assings the value to the variable
+y = "Those who know #{binary} and those who #{do_not}."
+
+#prints out the contents of the variable
+puts x
+#prints out the contents of the variable
+puts y
+
+#prints out the contents of the variable
+puts "I said #{x}"
+#prints out the contents of the variable©
+puts "I said '#{y}'"
+
+
+hilarious = false
+joke_evalutation = "Isn't that joke funny? #{hilarious}"
+
+w = "This is the left side of"
+e = "a string with a right side"
+
+#Concatenation—Returns a new String containing other_str concatenated to str.
+puts w + e
