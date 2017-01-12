3ab6c5247c5a36e123c4b9d9158dceaa47b02564
grekuna <gregor.martius@gmail.com>
2017-01-12 17:07:57 +0100

    Finish exercise 31

---
+++ ex31.rb
@@ -0,0 +1,62 @@
+puts " You enter a dark room with two doors. Do you go through door #1 or door #2?"
+
+print "> "
+door = $stdin.gets.chomp
+
+if door == "1"
+  puts "There's a gioan bear here eating a cheese cake. What do you do?"
+  puts "1. Take the cake."
+  puts "2. Scream at the bear."
+
+  print "> "
+  bear = $stdin.gets.chomp
+
+  if bear == "1"
+    puts "The bear eats your face off. Good job!"
+  elsif bear == "2"
+    puts "The bear eats your leg off. Good job!"
+  else
+    puts "Well, doing %s is probalby better. Bear runs away." % bear
+  end
+
+elsif door == "2"
+  puts "You stare into the endless abyss at Cthulhu's retina."
+  puts "1. Blueberries."
+  puts "2. Yellow jackets clothespins."
+  puts "3. Understanding revolvers yelling melodies."
+
+  print "> "
+  insanity = $stdin.gets.chomp
+
+  if insanity == "1" || insanity == "2"
+    puts "Your body survives powered by a mind of jello. Good job!"
+  else
+    puts "The insanity tos your eyes into pool of muck. Good job!"
+  end
+
+  else
+    puts "You stumble around and fall on a knife and die. Good job!"
+  end
+
+
+  puts "Please choose your favorite candidate:
+  1. Clinton
+  2. Trump
+  "
+  candidate = $stdin.gets.chomp
+
+  if candidate == "1"
+    puts "Thanks for you choice, I will do a great job"
+    puts "Tell me your age"
+    age = $stdin.gets.chomp.to_i
+    if age < 30
+      puts "You are still young"
+    else
+      puts "You are old or stupid, or both"
+    end
+
+  elsif candidate == "2"
+    puts "Howdy, thanks for your choice, yours Donald"
+  else
+    puts "You should have entered 1 or 2"
+  end
