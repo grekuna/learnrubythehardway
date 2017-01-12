68a2562a2ec9981aaec8883682a68ffe257e133b
grekuna <gregor.martius@gmail.com>
2017-01-12 11:24:00 +0100

    Finish exercise 7

---
+++ ex7.rb
@@ -0,0 +1,18 @@
+puts "Mary has a little lamb"
+puts "It's fleece is as white as #{'snow'}"
+puts "And everywhere that Mary went"
+puts "."*10
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "b"
+end7 = "u"
+end8 = "r"
+end9 = "g"
+
+# print end1 + end2 + end3 + end4
+puts end1 + end2 + end3 + end4
+puts end5 + end6 + end7 + end8
