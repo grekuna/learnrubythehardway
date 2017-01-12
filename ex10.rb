d89fa4dbdd948a7e8c5ecd169370e127c25c9810
grekuna <gregor.martius@gmail.com>
2017-01-12 11:26:15 +0100

    Finish exercise 10

---
+++ ex10.rb
@@ -0,0 +1,19 @@
+# tabby_cat = "\tI'm tabbed in."
+# persian_cat = "I'm split\non a line"
+# backlash_cat = "I'm \\ a \\ cat"
+fat_cat = "
+I'll do list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"
+#
+# puts tabby_cat
+# puts persian_cat
+# puts backlash_cat
+ puts fat_cat
+#
+# puts" heelo
+# new line"x
+
+puts "hello\x99 ascii bell"
