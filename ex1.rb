e34eaf8ea7e9a7d10ee066e8a86c55e2221bd4b9
grekuna <gregor.martius@gmail.com>
2017-01-12 11:14:39 +0100

    Finish exercise 1

---
+++ ex1.rb
@@ -0,0 +1,9 @@
+puts "Hello World"
+puts "Hello Again"
+puts "Another line"
+puts "THe fourth Line"
+puts "This is 'the' end"
+puts 'this is "the" end'
+puts "two plus two #{4+3} "
+# puts "\n"
+puts "hello \n"
