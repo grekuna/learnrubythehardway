1c5c0ccdefb57b88c712a700cda4bdb10c43119d
grekuna <gregor.martius@gmail.com>
2017-01-12 16:11:10 +0100

    Finish ex29

---
+++ ex29.rb
@@ -0,0 +1,35 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats
+  puts "Too many cats! The world is doomed!"
+end
+
+if people > cats
+  puts "Not many cat's! The world is saved!"
+end
+
+if people < dogs
+  puts "The world is droobled on!"
+end
+
+if people > dogs
+  puts "The world is dry!"
+end
+
+
+
+dogs += 5
+
+if people >= dogs
+  puts "People are greated than or equal to dogs."
+end
+
+if people <=dogs
+  puts "People are less than or equal to dogs."
+end
+
+if people == dogs
+  puts "People are dogs."
+end
