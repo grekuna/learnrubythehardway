8f523046fe89e4838e47cbbd04bb2c82196e92d5
grekuna <gregor.martius@gmail.com>
2017-01-12 11:22:39 +0100

    Finish exercise 5

---
+++ ex5.rb
@@ -0,0 +1,8 @@
+my_name = 'gregor'
+my_age = 29 #that was true in 2009
+my_height = 74 #inches
+my_eyes = 'blue'
+
+puts "let's talk about #{my_name}"
+puts "He's #{my_height} inches tall"
+puts "If I add #{my_age} and #{my_height} I get #{my_age + my_height}."
