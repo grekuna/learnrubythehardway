00ddb48c9068e475fbd3335bbf905f65107342f0
grekuna <gregor.martius@gmail.com>
2017-01-12 11:21:40 +0100

    Finisch exercise 3

---
+++ ex3.rb
@@ -0,0 +1,35 @@
+#“prints out the sentence”
+puts "I will now count my chickens"
+
+#“prints out the sentence and computes the value in the curley braces”
+puts "Hens #{25 +30 / 6}"
+#“prints out the sentence and computes the value in the curley braces”
+puts "Roosters #{100 -25 * 3 % 4}"
+
+#“prints out the sentence”
+puts "I will now count my eggs:"
+
+puts 3+2+1-5+4%2-1/4+6
+
+#“prints out the sentence”
+puts "Is it true that 3+2<5-7?"
+
+puts 3+2<5-7
+
+#“prints out the sentence and computes the value in the curley braces”
+puts "What is 3+2 #{3+2}"
+#“prints out the sentence and computes the value in the curley braces”
+puts "What is 5-7 #{5-7}"
+
+#“prints out the sentence”
+puts "Oh, that's why it's false"
+
+#“prints out the sentence”
+puts "How about some more?"
+
+#“prints out the sentence and computes the value in the curley braces”
+puts "Is it greater? #{5>-2}"
+#“prints out the sentence and computes the value in the curley braces”
+puts "Is it greater or equal? #{5>=-2}"
+#“prints out the sentence and computes the value in the curley braces”
+puts "Is it less or equal? #{5<=-2}"
