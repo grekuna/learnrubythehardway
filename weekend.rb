24632d16a048c1c01d245b1a739de78340edef3e
grekuna <gregor.martius@gmail.com>
2017-01-13 11:25:38 +0100

    Create file with weekend todos

---
+++ weekend.rb
@@ -0,0 +1,19 @@
+#define an array with five numbers
+
+#define an array with five strings
+
+#define an array with five hash
+
+#go through the arry with the each method and an
+# do block, print out each element if it is a strings
+#if the elements are numbers, double them and print them out
+#if the array holds neither strings nor ints, print "Can't read this"
+
+
+#define a method that prints an array using a block
+
+
+
+
+
+#run the method with the two arrays defined at the beginning
